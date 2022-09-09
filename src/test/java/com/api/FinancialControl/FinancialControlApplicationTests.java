fe1171aefe047574443c23f00ead55b6e5f867a1
alxc89 <alexcaardoso@gmail.com>
2022-09-09 16:57:10 -0300

    initial

---
+++ src/test/java/com/api/FinancialControl/FinancialControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.FinancialControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
